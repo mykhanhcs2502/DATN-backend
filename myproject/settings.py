3e49c3b3d9b8d794a5226bd2bb1550ea34dbaaf4
mykhanhcs2502 <mykhanhcs2502@gmail.com>
2024-05-17 02:51:34 +0700

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-ALLOWED_HOSTS+=os.environ.get("ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = []
 
 # Application definition
 
